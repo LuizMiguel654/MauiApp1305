b85adb6c554e9548e04591345a7b271ecdb883fa
LuizMiguel654 <dzzin520@gmail.com>
2025-05-13 11:11:43 -0300

    add atualização

---
+++ DadosUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1305
+{
+    public class DadosUsuario
+    {
+
+        public string Usuario {  get; set; }
+        public string Senha {  get; set; }
+
+    }
+}
--- Login.xaml.cs
+++ Login.xaml.cs
@@ -6,4 +6,56 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try {
+		
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>();
+			{
+
+
+				new DadosUsuario()
+				{
+					Usuario = "jose",
+					Senha = "123"
+				};
+
+				new DadosUsuario()
+				{
+					Usuario = "Nathan",
+					Senha = "321"
+				};
+
+			};
+
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+			//LINQ
+
+			if (lista_usuarios.Any(i => dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha))
+			
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+				App.Current.MainPage = new Protegida();
+			}
+			
+			else
+
+			{
+
+				throw new Exception("usuario ou senha incorreto");
+			}
+			
+			} catch (Exception ex) 
+		// catch baertura
+		{
+			   await DisplayAlert("iiiih", ex.Message, "Fechar");
+		} 
+		//catch fechamento
+    }
 }
